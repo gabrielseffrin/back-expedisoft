fe27acb9e1d3516dc5862a3765d9c95275d7cbf9
gabrielseffrin <gabrielfseffrin@gmail.com>
2026-02-23 21:26:23 -0300

    feat: added swagger docs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 use Psy\Util\Json;
+use OpenApi\Attributes as OA;
 
 class AuthController extends Controller
 {
 
-    public function __construct(private readonly AuthService $authService){}
+    #[OA\Post(
+        path: "/api/login",
+        description: "Endpoint para autenticação de usuários e geração de tokens de acesso.",
+        summary: "Autenticação de usuário",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ["email", "password"],
+                properties: [
+                    new OA\Property(property: "email", type: "string", format: "email", example: "email@teste.com"),
+                    new OA\Property(property: "password", type: "string", format: "password", example: "senha123")
+                ])),
+        tags: ["Authentication"],
+        responses: [
+            new OA\Response(response: 200, description: "Autenticação bem-sucedida, token gerado"),
+            new OA\Response(response: 401, description: "Credenciais inválidas"),
+            new OA\Response(response: 500, description: "Erro interno do servidor")
+        ])
+    ]
+    public function __construct(private readonly AuthService $authService)
+    {
+    }
 
     public function login(Request $request): \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
     {
--- app/Http/Controllers/Integration/UserController.php
+++ app/Http/Controllers/Integration/UserController.php
@@ -6,6 +6,10 @@ use App\Exceptions\IntegrationException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Integration\UserIntegrationRequest;
 use App\Services\UserService;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+use OpenApi\Attributes as OA;
 
 class UserController extends Controller
 {
@@ -16,10 +20,41 @@ class UserController extends Controller
     {
     }
 
+    #[OA\Post(
+        path: "/api/integration/user",
+        summary: "Create or update a user",
+        requestBody: new OA\RequestBody(
+            required: true,
+            content: new OA\JsonContent(
+                required: ["source_system", "user"],
+                properties: [
+                    new OA\Property(property: "source_system", type: "string", example: "SAP"),
+                    new OA\Property(
+                        property: "user",
+                        required: ["external_id", "name", "email"],
+                        properties: [
+                            new OA\Property(property: "external_id", type: "string", example: "12345"),
+                            new OA\Property(property: "name", type: "string", example: "John Doe"),
+                            new OA\Property(property: "email", type: "string", example: "teste@email.com"),
+                        ],
+                        type: "object"
+                    )
+                ]
+            )
+        ),
+        tags: ["Integration"],
+        responses: [
+            new OA\Response(response: 201, description: "User created or updated successfully"),
+            new OA\Response(response: 422, description: "Validation error"),
+            new OA\Response(response: 500, description: "Internal server error")
+        ]
+    )]
     /**
-     * @throws \Exception
+     * @param UserIntegrationRequest $request
+     * @return JsonResponse
+     * @throws Exception
      */
-    public function storeUser(UserIntegrationRequest $request): \Illuminate\Http\JsonResponse
+    public function storeUser(UserIntegrationRequest $request): JsonResponse
     {
         try {
             $user = $this->userService->storeUser($request->validated());
@@ -37,7 +72,7 @@ class UserController extends Controller
                 'errors' => $e->getErrors()
             ], $e->getHttpStatus());
 
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return response()->json([
                 'success' => false,
                 'message' => 'Internal server error'
