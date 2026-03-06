bc71b87fbbc8f7c44f6217b7159fffe191873be8
gabrielseffrin <gabrielfseffrin@gmail.com>
2026-03-05 22:14:27 -0300

    fix: parameter type

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -7,12 +7,12 @@ use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class OrderService
 {
-    public function findOrderById(int $orderId): LoadingOrder
+    public function findOrderById(string $orderId): LoadingOrder
     {
         return LoadingOrder::with('customer', 'destination', 'carrier', 'driver', 'vehicle', 'operator', 'dock', 'items.product', 'items.packages')->where('external_id', $orderId)->firstOrFail();
     }
 
-    public function getAllOrders(int $perPage): LengthAwarePaginator
+    public function getAllOrders(string $perPage): LengthAwarePaginator
     {
         return LoadingOrder::with('customer', 'destination', 'carrier', 'driver', 'vehicle', 'operator', 'dock', 'items.product', 'items.packages')->paginate($perPage);
     }
