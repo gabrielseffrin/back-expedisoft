7acd3165cbc21e7391f44db5b1d00a15d0611558
gabrielseffrin <gabrielfseffrin@gmail.com>
2026-03-10 22:22:36 -0300

    feat(health): added health endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HealthController;
 use App\Http\Controllers\Integration\LoadingOrderController;
 use App\Http\Controllers\Integration\UserController;
 use App\Http\Controllers\OrderController;
